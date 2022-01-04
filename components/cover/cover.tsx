c263bf4b7a4967b1f01d93ec30add37bfb2a203d
Heng Ngounse <hengngoungse@gmail.com>
2022-01-04 20:31:31 +0700

    fixed cover  v0.0.8

---
+++ components/cover/cover.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+interface IProps {
+  children: any;
+}
+const Cover = React.memo((props: IProps) => {
+  return <></>;
+});
+export default Cover;
