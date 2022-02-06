e58ffbe8fb1980cf85db0510b8fd757463424236
Heng Ngounse <hengngoungse@gmail.com>
2022-02-06 15:33:49 +0700

    component cover  v0.0.15

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,18 @@
 import type { NextPage } from "next";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 2000);
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
