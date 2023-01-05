116cb9d45ea3e3b82922c9f0ee0031ba2ead6c00
Ngounse HENG <hengngoungse@gmail.com>
2023-01-05 16:09:51 +0700

    fixed deploy v0.0.1.5:16

---
+++ src/utils.ts
@@ -0,0 +1,26 @@
+export const menuHome = [
+  {
+    name: "Spotify",
+    href: "/spotify",
+    desc: "Find in-depth information about spotify features and API.",
+    bgImg: "",
+  },
+  {
+    name: "Learn",
+    href: "",
+    desc: "Learn about Next.js in an interactive course with quizzes!",
+    bgImg: "",
+  },
+  {
+    name: "Examples",
+    href: "",
+    desc: "Discover and deploy boilerplate example Next.js projects.",
+    bgImg: "",
+  },
+  {
+    name: "Deploy",
+    href: "https://vercel.com/",
+    desc: "Instantly deploy your Next.js site to a public URL with Vercel.",
+    bgImg: "",
+  },
+];
